ae35681615cdc67e630034d692651b2d3b1e9cd5
Vesa Koskela <a.vesa.e.koskela@gmail.com>
2021-06-12 21:16:26 +0300

    Add doomOneCharacter

--- src/mechanics/challenges/doomroom.ts
+++ src/mechanics/challenges/doomroom.ts
@@ -6,3 +6,7 @@ export function doomEveryone(time_to_doom: number): void {
 	if (Characters.Thinker.HP  > 0) Characters.Thinker.setDoom(time_to_doom);
 	if (Characters.Tinkerer.HP > 0) Characters.Tinkerer.setDoom(time_to_doom);
 }
+
+export function doomOneCharacter(time_to_doom: number, character: Characters.Character): void {
+	if (character.HP > 0) character.setDoom(time_to_doom);
+}
