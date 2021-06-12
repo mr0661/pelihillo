c9384fd757cee5317b63fc4d2c7237639ee39145
Vesa Koskela <a.vesa.e.koskela@gmail.com>
2021-06-12 15:35:25 +0300

    Add Dragon challenge

--- src/mechanics/challenges.ts
+++ src/mechanics/challenges.ts
@@ -21,3 +21,22 @@ craft_result:
 	, resolution: "TODO"
 	};
 }
+
+export class Dragon extends Core.Challenge {
+strength_result:
+	{ damage: 7
+	, resolution: "The goblin battles bravely against the dragon and is able to subdue her, but is severely wounded."
+	};
+precision_result:
+	{ damage: 2
+	, resolution: "The goblin was able to shoot an arrow into the dragon's weak spot, killing her with only minor wounds."
+	};
+smarts_result:
+	{ damage: 5
+	, resolution: "The goblin outsmarted the dragon by having her breathe fire on the weak spot, but suffered moderate wounds from the efforts."
+	};
+craft_result:
+	{ damage: 10
+	, resolution: "The goblin was able to collapse the ceiling on the dragon, but was also buried."
+	};
+}
