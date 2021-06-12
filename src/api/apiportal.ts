12e388e8f650bb90a3f74967b7e011f521b64cfe
scor <kimmo.kuohula@gmail.com>
2021-06-12 22:47:08 +0300

    TypeScript middlehandler

---
+++ src/api/apiportal.ts
@@ -0,0 +1,16 @@
+import { getTexts, postNewText, upVoteText, downVoteText } from './api.js';
+
+export async function getNotes(id: number){
+	return await getTexts(id);
+};
+
+export async function getNewNote(id: number){
+	return await postNewText(id);
+};
+export async function upVoteNote(id: number, msg: string){
+	const message: object = {note: msg};
+	return await upVoteText(id, message);
+};
+export async function downVoteNote(id: number){
+	return await downVoteText(id);
+};
