a3f33660082110b5a333a1c173ceb72504420ed1
Vesa Koskela <a.vesa.e.koskela@gmail.com>
2021-06-12 13:49:18 +0300

    Add doom counter

--- src/mechanics/character.ts
+++ src/mechanics/character.ts
@@ -4,11 +4,28 @@ export class Character {
 	readonly primarySkill: Skill;
 	readonly secondarySkill: Skill;
 	HP: number;
+	doom_track?: number;
 
 	constructor(primary: Skill, secondary: Skill) {
 		this.primarySkill   = primary;
 		this.secondarySkill = secondary;
 		this.HP = 10;
+	}
+
+	setDoom(counter: number): void {
+		this.doom_track = counter;
+	}
+
+	getDoom(): string {
+		if (this.doom_track == undefined) return "Not doomed yet.";
+		return this.doom_track + " steps until DOOM!";
+	}
+
+	countdownToDoom(): boolean {
+		if (this.doom_track == undefined) return false;
+
+		--this.doom_track;
+		return (this.doom_track == 0);
 	}
 }
 
