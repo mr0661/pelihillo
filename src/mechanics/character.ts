5e404e8ff036206b197d9737e3636bf3c7935eea
Vesa Koskela <a.vesa.e.koskela@gmail.com>
2021-06-12 12:39:14 +0300

    Add characters

--- src/mechanics/character.ts
+++ src/mechanics/character.ts
@@ -12,3 +12,7 @@ export class Character {
 	}
 }
 
+export let Fighter  = new Character(Skill.Strength , Skill.Craft    );
+export let Rogue    = new Character(Skill.Precision, Skill.Strength );
+export let Thinker  = new Character(Skill.Smarts   , Skill.Precision);
+export let Tinkerer = new Character(Skill.Craft    , Skill.Smarts   );
