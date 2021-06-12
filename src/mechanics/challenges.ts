3f2f2a52859e4613433f15ef0405a153b272626a
Vesa Koskela <a.vesa.e.koskela@gmail.com>
2021-06-12 15:35:06 +0300

    Refine Magical_Trap challenge

--- src/mechanics/challenges.ts
+++ src/mechanics/challenges.ts
@@ -1,12 +1,23 @@
 import * as Core from "./core";
 
 export class Magical_Trap extends Core.Challenge {
-	strength_result: 	{ damage: 10
-		, resolution: "The blunt force of the trap is absorbed by the goblin, making the room safe."};
-	precision_result:	{ damage: 2
-		, resolution: "TODO"};
-	smarts_result:		{ damage: 0
-		, resolution: "TODO"};
-	craft_result:		{ damage: 5
-		, resolution: "TODO"};
+strength_result:
+	{ damage: 10
+	, resolution: "The blunt force of the trap is absorbed by the goblin, making the room safe."
+	};
+
+precision_result:
+	{ damage: 2
+	, resolution: "TODO"
+	};
+
+smarts_result:
+	{ damage: 0
+	, resolution: "TODO"
+	};
+
+craft_result:
+	{ damage: 5
+	, resolution: "TODO"
+	};
 }
