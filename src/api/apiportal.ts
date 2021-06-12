3c23906e3b1c293a37021e3b97f78fc55245661e
scor <kimmo.kuohula@gmail.com>
2021-06-12 23:44:30 +0300

    Fixing and fine tuning front api commands.

--- src/api/apiportal.ts
+++ src/api/apiportal.ts
@@ -4,13 +4,15 @@ export async function getNotes(id: number){
 	return await getTexts(id);
 };
 
-export async function postNewNote(id: number){
-	return await postNewText(id);
-};
-export async function upVoteNote(id: number, msg: string){
+export async function postNewNote(id: number, msg: string){
 	const message: object = {note: msg};
-	return await upVoteText(id, message);
+	return await postNewText(id, message);
 };
+
+export async function upVoteNote(id: number){
+	return await upVoteText(id);
+};
+
 export async function downVoteNote(id: number){
 	return await downVoteText(id);
 };
