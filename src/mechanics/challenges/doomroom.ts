810469e258ce06b9edeb41c8365e1e3464a6d07c
Vesa Koskela <a.vesa.e.koskela@gmail.com>
2021-06-12 21:17:55 +0300

    Add doom countdown

--- src/mechanics/challenges/doomroom.ts
+++ src/mechanics/challenges/doomroom.ts
@@ -10,3 +10,14 @@ export function doomEveryone(time_to_doom: number): void {
 export function doomOneCharacter(time_to_doom: number, character: Characters.Character): void {
 	if (character.HP > 0) character.setDoom(time_to_doom);
 }
+
+export function doomCountdown(): boolean {
+	let party_alive = false;
+
+	if (Characters.Fighter.HP  > 0 && !Characters.Fighter.countdownToDoom() ) party_alive = true;
+	if (Characters.Ranger.HP   > 0 && !Characters.Ranger.countdownToDoom()  ) party_alive = true;
+	if (Characters.Thinker.HP  > 0 && !Characters.Thinker.countdownToDoom() ) party_alive = true;
+	if (Characters.Tinkerer.HP > 0 && !Characters.Tinkerer.countdownToDoom()) party_alive = true;
+
+	return party_alive;
+}
