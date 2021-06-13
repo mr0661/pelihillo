75956b1006895f666448ffe77e66b10c935a2094
Vesa Koskela <a.vesa.e.koskela@gmail.com>
2021-06-13 12:59:58 +0300

    Add Yellow Ooze

---
+++ src/mechanics/challenges/yellow_ooze.ts
@@ -0,0 +1,32 @@
+/**
+ * Challenge template file. Copy this and change into a new challenge.
+ */
+
+import * as Core from "../core";
+
+let strength_result: Core.ActionResult =
+{ damage: 2
+, resolution: "TODO"
+};
+
+let precision_result: Core.ActionResult =
+{ damage: 3
+, resolution: "TODO"
+};
+
+let smarts_result: Core.ActionResult =
+{ damage: 0
+, resolution: "The goblin blasts the ooze with a fire ball, destroying it."
+};
+
+let craft_result: Core.ActionResult =
+{ damage: 1
+, resolution: "TODO"
+};
+
+let image_loc  : string = "data/challenges/ooze.png";
+let description: string = "A pool of yellow ooze sits in the middle of the room. It seems to resist physical damage.";
+
+export const Yellow_Ooze = new Core.Challenge(
+	strength_result, precision_result, smarts_result, craft_result, image_loc, description
+);
