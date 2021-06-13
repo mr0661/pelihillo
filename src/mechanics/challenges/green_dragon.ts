46c0b0523bbf1b3b6a63f1ac7e06e48861b285a7
Vesa Koskela <a.vesa.e.koskela@gmail.com>
2021-06-13 11:48:44 +0300

    Add Green Dragon

---
+++ src/mechanics/challenges/green_dragon.ts
@@ -0,0 +1,28 @@
+import * as Core from "../core";
+
+let strength_result: Core.ActionResult =
+{ damage: 2
+, resolution: "The dragon was imposing, but surprisingly weak. The goblin was able to defeat him with minor wounds."
+};
+
+let precision_result: Core.ActionResult =
+{ damage: 5
+, resolution: "The goblin was able to shoot out the dragon's eyes, but got a face full of dragon's breath for his efforts."
+};
+
+let smarts_result: Core.ActionResult =
+{ damage: 10
+, resolution: "The dragon ate half of the goblin, and left the room after having his fill."
+};
+
+let craft_result: Core.ActionResult =
+{ damage: 7
+, resolution: "The goblin was able to build a trap around the dragon and cage it, but was severely wounded in the process."
+};
+
+let image_loc  : string = "data/challenges/dragon.png";
+let description: string = "The party encounters an imposing green dragon. The magical energy of the room feels drained.";
+
+export const Green_Dragon = new Core.Challenge(
+	strength_result, precision_result, smarts_result, craft_result, image_loc, description
+);
