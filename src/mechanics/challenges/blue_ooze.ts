1df73945fa50ea0950cb7f0b70fbb3bce8c844ba
Vesa Koskela <a.vesa.e.koskela@gmail.com>
2021-06-13 12:56:00 +0300

    Add Blue Ooze

---
+++ src/mechanics/challenges/blue_ooze.ts
@@ -0,0 +1,32 @@
+/**
+ * Challenge template file. Copy this and change into a new challenge.
+ */
+
+import * as Core from "../core";
+
+let strength_result: Core.ActionResult =
+{ damage: 3
+, resolution: "TODO"
+};
+
+let precision_result: Core.ActionResult =
+{ damage: 0
+, resolution: "The goblin shoots the hole and water bursts out, sweeping the ooze into a crack in the floor."
+};
+
+let smarts_result: Core.ActionResult =
+{ damage: 1
+, resolution: "TODO"
+};
+
+let craft_result: Core.ActionResult =
+{ damage: 2
+, resolution: "TODO"
+};
+
+let image_loc  : string = "data/challenges/ooze.png";
+let description: string = "A pool of blue ooze sits in the middle of the room. There's water dripping from a hole on the ceiling.";
+
+export const Blue_Ooze = new Core.Challenge(
+	strength_result, precision_result, smarts_result, craft_result, image_loc, description
+);
